0d10124756d6b44b34c780243f655b816fe04925
Jon Alm Eriksen <jon.alm.eriksen@protonmail.com>
2020-01-19 20:04:05 +0100

    add a bunch

--- src/EventTimeSeries.jl
+++ src/EventTimeSeries.jl
@@ -1,15 +1,21 @@
 module EventTimeSeries
 
-export Event,
-       EventTS,
-       duration,
-       events,
-       timestamps,
-       values,
-       tags,
-       drop_repeated,
-       fill_forward_group_tags,
-       split_tags
+using Base.Iterators: flatten, repeated
+
+export
+    # types
+    Event,
+    EventTS,
+    # EventTS methods
+    duration,
+    rows,
+    timestamps,
+    tags,
+    values,
+    drop_repeated,
+    split,
+    merge,
+    fill_forward_group_tags
 
 
 ###
@@ -36,60 +42,130 @@ end
 
 
 ###
-### Single Event
+### Tag
 ###
 
-struct Event{S, T, U}
-    timestamp::S
-    tag::T
-    value::U
+abstract type TagType end
+struct SeriesTag <: TagType end
+struct EventTag <: TagType end
+
+for T in [Symbol, Char, String, Tuple, NamedTuple, Number]
+    @eval tagtype(::$T) = EventTag()
 end
 
-Base.show(io::IO, e::Event) = print(io, "time: $(e.timestamp), tag: $(e.tag), val: $(e.value)")
+for T in [AbstractVector, AbstractRange]
+    @eval tagtype(::$T) = SeriesTag()
+end
+
+tagtype(v::T) where {T} = @error """
+In order to use $v as a tag, define the 'tagtype' trait:
+'EventTimeSeries.tagtype(::$T) = EventTag()' or
+'EventTimeSeries.tagtype(::$T) = SeriesTag()'
+"""
+
+
+###
+### Event
+###
+
+struct Event{T, V}
+    timestamp::T
+    value::V
+end
+
+Base.show(io::IO, e::Event) = print(io, "time: $(e.timestamp), val: $(e.value)")
 
 
 ###
 ### Event Time Series
 ###
 
-struct EventTS{V}
-    series::V
-    function EventTS(series::V; validate=true) where {V}
-        if validate
-            timestamps = getfield.(series, :timestamp)
-            @assert issorted(timestamps)
-            eltypes = typeof.(series)
-            @assert all(hasfield.(eltypes, :value))
-            @assert all(hasfield.(eltypes, :tag))
-        end
-        new{V}(series)
+struct EventTS{T, V, U}
+    series::Vector{Event{T, V}}
+    tag::U
+    function EventTS(series::Vector{Event{T, V}}, tag::U) where {T, V, U}
+        validate_tag && _validate_tag(series, tag)
+        new{T, V, U}(series, tag)
     end
 end
 
-EventTS(timestamps, tags, values) = EventTS([Event(ts, tag, v) for (ts, tag, v) in zip(timestamps, tags, values)])
-EventTS(timestamps, values; tag)  = EventTS([Event(ts, tag, v) for (ts, v) in zip(timestamps, values)])
-
-Base.IndexStyle(::EventTS)             = LinearIndices()
-Base.length(ts::EventTS)               = length(ts.series)
-Base.setindex!(ts::EventTS, v, i)      = setindex!(ts.series, v, i)
-Base.getindex(ts::EventTS, i::Integer) = getindex(ts.series, i)
-Base.getindex(ts::EventTS, i)          = EventTS(getindex(ts.series, i))
-Base.iterate(ts::EventTS, el)          = iterate(ts.series, el)
-Base.filter(f, ts::EventTS)            = filter(f, ts.series)
-
-function Base.merge(ts1::EventTS, ts2::EventTS)
-    merged = vcat(ts1.series, ts2.series)
-    sort!(merged, by=x->x.timestamp)
-    EventTS(merged, validate=false)
+_validate_tag(series, tag) = validate_tag(series, tag, tagtype(tag))
+_validate_tag(series, tag, ::EventTag) = nothing
+function _validate_tag(series, tag, ::SeriesTag)
+    @assert length(series) == length(tag)
+    @assert tagtype(eltype(tag)) == EventTag
 end
 
-events(ts::EventTS)     = (e for e in ts.series)
-timestamps(ts::EventTS) = (e.timestamp for e in events(ts))
-tags(ts::EventTS)       = (e.tag for e in events(ts))
-values(ts::EventTS)     = (e.value for e in events(ts))
-duration(ts::EventTS)   = ts.series.timestamps[end] - ts.series.timestamps[1]
+function EventTS(;timestamps, values, tag)
+    @assert issorted(timestamps)
+    @assert length(timestamps) == length(values)
+    series = [Event(ts, v) for (ts, v) in zip(timestamps, values)]
+    _validate_tag(series, tag)
+    EventTS(series, tag)
+end
 
-function drop_repeated(ts::EventTS, keep_tail=true)
+tagtype(s::EventTS) = tagtype(s.tag)
+rows(ts::EventTS) = @inbounds (ts[i] for i in 1:length(ts))
+timestamps(ts::EventTS) = (e.time for e in rows(ts))
+tags(ts::EventTS) = tags(ts::EventTS, tagtype(ts))
+tags(ts::EventTS, ::EventTag) = repeated(ts.tag, length(ts))
+tags(ts::EventTS, ::SeriesTag) = ts.tag
+values(ts::EventTS) = (e.val for e in rows(ts))
+duration(ts::EventTS) =  ts.series.timestamps[end] - ts.series.timestamps[1]
+
+Base.IndexStyle(::EventTS) = LinearIndices()
+Base.length(ts::EventTS) = length(ts.series)
+Base.getindex(ts::EventTS, i) = _getindex(ts.series, i, tagtype(ts))
+
+# returns a new EventTS containing subset of values defined by s
+_getindex(ts::EventTS, s, ::EventTag) = EventTS(ts.tag, ts.series[s])
+_getindex(ts::EventTS, s, ::SeriesTag) = EventTS(ts.tag[s], ts.series[s])
+
+# return a single (timestamp, tag, value) triple
+function _getindex(ts::EventTS, i::Integer, ::EventTag)
+    (time=ts.series[i].timestamp, tag=ts.tag, val=ts.series[i].value)
+end
+
+function _getindex(ts::EventTS, i::Integer, ::SeriesTag)
+    (time=ts.series[i].timestamp, tag=ts.tag[i], val=ts.series[i].value)
+end
+
+# timestamps must have equal types
+function Base.merge(ts1::EventTS{T}, ts2::EventTS{T}) where {T}
+    _merge(ts1, tagtype(ts1), ts2, tagtype(ts2))
+end
+
+function _series_sortperm(ts1, ts2)
+    series = flatten((ts1.series, ts2.series)) |> collect
+    series, sortperm(series, by=x->x.timestamp)
+end
+
+function _merge(ts1, ::EventTag, ts2, ::EventTag)
+    series, perm = _series_sortperm(ts1, ts2)
+    if ts1.tag == ts2.tag
+        return EventTs(series[perm], t1)
+    else
+        tag = flatten((repeated(ts1.tag, length(t1)), repeated(ts2.tag, length(t2))))
+        return  EventTs(series[perm], tag[perm])
+    end
+end
+
+_merge(ts1, ::EventTag, tag2::SeriesTag) = _merge(ts2, tagtype(ts2), ts1, tagtype(ts1))
+function _merge(ts1, ::SeriesTag, ts2, ::EventTag)
+    series, perm = _series_sortperm(ts1, ts2)
+    tag = flatten()
+end
+
+function Base.merge(ts1::EventTS{T}, ts2::EventTS{T}) where {T}
+    series, perm = _series_sortperm(ts1, ts2)
+end
+
+_merge()
+
+
+drop_repeated(ts::EventTS; kwargs...) = drop_repeated(ts, tagtype(ts); kwargs...)
+
+function drop_repeated(ts::EventTS, ::TSingle; keep_tail=true)
     keep = BitVector(undef, length(ts))
     fill!(keep, false)
 
@@ -131,7 +207,7 @@ function fill_forward_group_tags(ts::EventTS; skip_initial_missing=false)
     EventTS(out, validate=false)
 end
 
-split_tags(ts::EventTS) = (t => filter(x->x.tag==t, ts) for t in unique(tags(ts)))
+#split_tags(ts::EventTS) = (t => filter(x->x.tag==t, ts) for t in unique(tags(ts)))
 
 
 
